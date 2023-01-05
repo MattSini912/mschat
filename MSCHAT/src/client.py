5b9ce8f3aa8a3df9c492d998394786f607353df2
Matteo <106877447+MattSini912@users.noreply.github.com>
2023-01-05 12:38:43 +0100

    1.5.0 Update
    - New logo and icon
    - Added version compatibility check
    - Improved GUI window
    - Added restore mode to client and file download from GitHub
    - Added port selection to autoload
    - Improved Unicode support
    - Major bug fixes

--- MSCHAT/src/client.py
+++ MSCHAT/src/client.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import requests
+import urllib.request
 import time
 from socket import *
 import hashlib
@@ -16,6 +19,58 @@ from mycryptfunc import *
 import PySimpleGUI as sg
 from queue import Queue
 
+import pywintypes
+import win32con
+import win32gui
+
+client_version = "1.5.0" #<---------------------------------
+
+_r = "\033[1;31m" #red
+_g = "\033[1;32m" #green
+_b = "\033[1;34m" #blue
+_w = "\033[1;37m" #white
+_bkw = "\033[30;107m" #black on white
+_e = "\033[0m" #end
+
+def random_logo():
+    logo = 0
+
+    if os.name == 'nt':
+        logo = random.randint(0,5)
+    else:
+        print("MSCHAT by MattSini912")
+        return
+
+    _ms, _ch, _at = _w, _w, _w
+    if logo == 1:
+        _ms, _ch, _at = _b, _b, _b
+    elif logo == 2:
+        _ms, _ch, _at = _g, _w, _r
+    elif logo == 3:
+        _ms, _ch, _at = _r, _r, _r
+    elif logo == 4:
+        _ms, _ch, _at = _g, _g, _g
+    elif logo == 5:
+        _ms, _ch, _at = _bkw, _bkw, _bkw
+
+    print(_ms + "                " + _e + _ch + "     _     " + _e + _at + "      _    " + _e)
+    print(_ms + "  _ __ ___  ___ " + _e + _ch + " ___| |__  " + _e + _at + " __ _| |_  " + _e)
+    print(_ms + " | '_ ` _ \/ __|" + _e + _ch + "/ __| '_ \ " + _e + _at + "/ _` | __| " + _e)
+    print(_ms + " | | | | | \__ \\"+ _e + _ch + " (__| | | |" + _e + _at + " (_| | |_  " + _e)
+    print(_ms + " |_| |_| |_|___/" + _e + _ch + "\___|_| |_|" + _e + _at + "\__,_|\__| " + _e)
+    print(_ms + "                " + _e + _ch + "           " + _e + _at + "           " + _e)
+    print(f"                {client_version}")
+    print()
+    print(" https://github.com/MattSini912/mschat")
+    print(" " + 37*"_")
+
+#Flash window
+cur_window = None
+def flash(hwnd):
+        win32gui.FlashWindowEx(hwnd, win32con.FLASHW_ALL | win32con.FLASHW_TIMERNOFG, 0, 0)
+
+import traceback ###debug
+
 servers = ["SERVER"]
 admins = ["ADMIN", "SERVER"]
 
@@ -24,17 +79,8 @@ enable_gui = True
 # Start
 if os.name == 'nt':
     os.system("")
-    print("\033[1;34m" + "MSCHAT by MattSini912" + "\033[0m")
-else:
-    print("MSCHAT by MattSini912")
-
-hostip = input("Enter server's IP adress (default = 127.0.0.1) -> ")
-if hostip == "" or hostip.isspace():
-    hostip = '127.0.0.1'
-
-port = input("Enter server's port (default = 55555) -> ")
-if port == "" or port.isspace():
-    port = 55555
+ 
+random_logo()
 
 def hashpwd(password, times=1):
     for i in range(0,times):
@@ -43,7 +89,33 @@ def hashpwd(password, times=1):
         password = h.hexdigest()
     return h.hexdigest()
 
-mode = input("/login (default), /register, /delete or /nogui -> ").lower()
+def clear_name(name):
+    name = re.sub(r"[^a-zA-Z0-9 _àèìòù+&£$€@#]","",name)
+    name = name.strip()
+    name = name[:min(len(name), 20)]
+    name = name.strip()
+    name = name.replace(' ', '_')
+    return name
+
+mode = input('''
+Type one of the folowing options:
+
+ /login         (DEFAULT, connect to a server)
+ /register      (register to a server that uses authentication)
+ /delete        (delete your profile from a server)
+ /nogui         (login mode without GUI, you'll use this terminal to chat)
+ /restore       (delete 'server' folder and download it again from GitHub)
+
+---> ''').lower()
+
+if "restore" not in mode:
+    hostip = input("Enter server's IP address (default = 127.0.0.1) -> ")
+    if hostip == "" or hostip.isspace():
+        hostip = '127.0.0.1'
+
+    port = input("Enter server's port (default = 55555) -> ")
+    if port == "" or port.isspace():
+        port = 55555
 
 if "login" in mode:
     operation = 'LOGIN'
@@ -53,17 +125,12 @@ elif "register" in mode:
     enable_gui = False
     operation = 'REGISTER'
     name_to_reg = input("Choose a nickname -> ")
-    name_to_reg = re.sub(r"[^a-zA-Z0-9 _àèìòù+&£$€@#]","",name_to_reg)
-    name_to_reg = name_to_reg.strip()
+    name_to_reg = clear_name(name_to_reg)
     if name_to_reg == "" or name_to_reg.isspace() or name_to_reg == ": ":
         name_to_reg = "user" + str(random.randint(10000, 99999))
-    name_to_reg = name_to_reg[:min(len(name_to_reg), 20)]
-    name_to_reg = name_to_reg.strip()
-    name_to_reg = name_to_reg.replace(' ', '_')
 
     mail_to_reg = input("Email -> ")
-    mail_to_reg = re.sub(r"[^a-zA-Z0-9_~+@\.\-]","",mail_to_reg)
-    mail_to_reg = mail_to_reg.strip()
+    mail_to_reg = re.sub(r"[^a-zA-Z0-9_~+@\.\-]","",mail_to_reg).strip()
 
     pwd_to_reg = input("Password -> ")
     pwd_to_reg = hashpwd(pwd_to_reg, 100000)
@@ -74,14 +141,57 @@ elif "delete" in mode:
     enable_gui = False
     operation = 'DELETE'
     mail_to_del = input("Email -> ")
-    mail_to_del = re.sub(r"[^a-zA-Z0-9_~+@\.\-]","",mail_to_del)
-    mail_to_del = mail_to_del.strip()
+    mail_to_del = re.sub(r"[^a-zA-Z0-9_~+@\.\-]","",mail_to_del).strip()
     command = f'DELETE {mail_to_del}'
 
 elif "nogui" in mode:
     enable_gui = False
     operation = 'LOGIN'
     command = 'LOGIN'
+
+elif "restore" in mode:
+    folder_path = os.getcwd() + "\server"
+    if os.path.exists(folder_path):
+        print("\nClose all the files and executables from 'server' folder!\nDeletion will start in 15 seconds.")
+        time.sleep(15)
+        input("Press ENTER to start -> ")
+        print("Deleting... Do not close this window!")
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        #os.rmdir(folder_path)
+        print("Files deleted!")
+    else:
+        os.mkdir(folder_path)
+        print("Folder 'server' not found in the directory, a new one was created.")
+
+
+    url_path = 'https://raw.githubusercontent.com/MattSini912/mschat/main/MSCHAT/server/'
+    print("Downloading files from " + url_path)
+    files = requests.get(url_path + 'FILES')
+    filelist = files.text.split()
+    for filename in filelist:
+        print("Downloading " + filename)
+        f = urllib.request.urlopen(url_path + filename)
+        file = f.read()
+        f.close()
+        if filename.endswith('.exe'):
+            f2 = open(folder_path + "\ "[0] + filename, 'wb')
+            f2.write(file)
+        else:
+            f2 = open(folder_path + "\ "[0] + filename, 'w')
+            f2.write(str(file.decode('utf-8')).replace('\r',''))
+        f2.close()
+
+    input("Operation terminated!")
+    exit()
+    
 
 else:
     operation = 'LOGIN'
@@ -96,27 +206,23 @@ else:
     nickname = "isarandomnick"
 
 # Clean nickname
-nickname = re.sub(r"[^a-zA-Z0-9 _àèìòù+&£$€@#]","",nickname)
-nickname = nickname.strip()
+nickname = clear_name(nickname)
 if nickname == "" or nickname.isspace() or nickname == ": ":
     nickname = str(random.randint(10000, 99999))
-nickname = nickname[:min(len(nickname), 20)]
-nickname = nickname.strip()
-nickname = nickname.replace(' ', '_')
 
 if operation == 'LOGIN':
-    password = input("Password for profile (leave blank if not required) -> ")
+    password = input("- NOTE: some servers or profiles may require a password to connect, leave blank if it's not your case.\nPassword for profile -> ")
 else:
     password = "isarandompass"
 
 password = hashpwd(password, 100000)
 
 # Connecting To Server
+print()
 print("Connecting...")
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((hostip, int(port)))
 print("Making requests...")
-
 
 stop_thread = False
 tls_key = bytes(0)
@@ -157,13 +263,17 @@ def receive():
     while True:
         global stop_thread
         global tls_key
+        global window
         if stop_thread:
             break
         try:
             # Receive Message From Server
             message = client.recv(8192).decode('utf-8', 'replace')
-            #print(tls_key, message) #debug 
-            message = dc(message)
+            try: 
+                message = dc(message)
+            except: ###debug
+                defprint("Error while receiving a message") ###debug
+                continue
     
             if message == 'CLTKEY':
                 clt = DiffieHellman()
@@ -191,7 +301,19 @@ def receive():
                     client.send(ec(str(vercode)))
                     response = client.recv(1024).decode('utf-8', 'replace')
                     print(response)
+                    print()
 
+            elif message == 'CONNECTED':
+
+                if enable_gui:
+                    mygui_thread.start() #open the GUI
+
+                    if cur_window and os.name == 'nt' and win32gui.GetForegroundWindow() != cur_window:
+                        flash(cur_window)
+                        
+            elif message == 'ASKVER':
+                client.send(client_version.encode('utf-8', 'replace'))
+            
             elif message == 'CMD':
                 client.send(ec(command))
             
@@ -206,9 +328,21 @@ def receive():
                 client.close()
                 stop_thread = True
 
+            elif message == 'OLDVER':
+                print("This server is using a different version!")
+                client.close()
+                stop_thread = True
+            
             elif message == 'DOUBLE':
                 print("Another user with your data is already present!")
                 client.close()
+                stop_thread = True
+
+            elif message == 'KCK':
+                print("Kicked from the server!")
+                client.close()
+                if window:
+                    window.close()
                 stop_thread = True
 
             elif message == 'BAN':
@@ -229,8 +363,14 @@ def receive():
             else:
                 if message != "" and not message.isspace():
                     defprint(f"{timestamp()} {message}")
+                    try:
+                        if cur_window and os.name == 'nt' and win32gui.GetForegroundWindow() != cur_window:
+                            flash(cur_window)
+                    except:
+                        pass
 
         except:
+            traceback.print_exc() ###debug
             # Close Connection When Error
             print("An error occured!")
             client.close()
@@ -256,6 +396,7 @@ def write(txtbox):
             if message[skip:].startswith('/help'):
                 defprint('''
 LIST OF COMMANDS:              |PERMISS.|DESCRIPTION                 
+/source                         (client) Open the GitHub page of this project
 /help                           (client) Print the list of commands
 /settings help                  (server) Print the list of server's commands and settings
 /list                           (client) Print the list of online users
@@ -274,14 +415,21 @@ LIST OF COMMANDS:              |PERMISS.|DESCRIPTION
 /ban <user>                     (admins) Ban <user>
 /unban <user>                   (admins) Unban <user>
 /donut                          (donuts) It's a donut!
-/publicip                       (client) Print your IP adress
+/publicip                       (client) Print your IP address
 /ipconfig                       (client) Open terminal and run /ipconfig
-/ping <adress>                  (client) Get latency beetween your pc and <adress>
+/ping <address>                 (client) Get latency beetween your pc and <address>
 /credits                        (client) Show credits page
 /clear                          (client) Clear terminal
 /leave                          (client) Disconnect yourself
 
 ''')
+            elif message[skip:].startswith('/source'):
+                try:
+                    if os.name == 'nt':
+                        os.system('cmd /c "start https://github.com/MattSini912/mschat"')
+                except:
+                    defprint("¯\(°_o)/¯")
+            
             elif message[skip:].startswith('/settings'):
                 if nickname not in servers:
                     defprint(f"{timestamp()} Refused!")
@@ -419,6 +567,9 @@ WARNING! EDITING OR DELETING FILES FROM "SERVER" FOLDER (e.g. "login_details.txt
 
             elif message[skip:].startswith('/credits'):
                 defprint('''
+                                                    
+https://github.com/MattSini912/mschat
+
 Created by:                         Matteo Sinitò (MattSini912)    
 
 Testing:                            Luca Distefano
@@ -454,19 +605,24 @@ Original pydispo module:            Aakash Patil
 def mygui(txtbox):
     global window
     global stop_thread
+    global cur_window
     # Create the Window
     sg.theme('DarkBlue17')
     layout = [
-        [sg.Text('MSChat by MattSini912'), sg.Button('Clear Window'), sg.Button('Close Window')],
+        [sg.Text('MSChat by MattSini912'), sg.Button('Clear window'), sg.Button('Close window')],
         [sg.Text(f'Connected with: {hostip}'), sg.Button('Online users')],
         [sg.Text(f'Welcome back "{nickname}"! Check your terminal for more informations.')],
         [sg.Multiline(size=(60, 10), key='OUTPUT', disabled=True, autoscroll=True, expand_x=True, expand_y=True)],
         [sg.Button('C'), sg.Button('SEND'), sg.Button('/'), sg.Input(size=(45, 10), key='INPUT', expand_x=True)], 
         [sg.Button('HELP'), sg.Button('DM'), sg.Button('CREDITS')],  
         ]
-    window = sg.Window(f'MSChat: {nickname}', layout, size=(580, 450), resizable=True).Finalize()
+    window = sg.Window(f'MSChat: {nickname}', layout, size=(650, 500), resizable=True, font=('Consolas', 10)).Finalize()
     window.TKroot.minsize(480,350)
     window['INPUT'].bind("<Return>", "_Enter")
+    cur_window = int(window.TKroot.frame(), base=16) # window id
+    #window.minimize()
+    window.BringToFront()
+    window['INPUT'].set_focus()
 
     while True:
         if stop_thread:
@@ -511,6 +667,6 @@ receive_thread.start()
 write_thread = threading.Thread(target=write, args=(txtbox, ))
 write_thread.start()
 
-mygui_thread = threading.Thread(target=mygui, args=(txtbox, ))
-if enable_gui:
-    mygui_thread.start()
+mygui_thread = threading.Thread(target=mygui, args=(txtbox, )) #starts when connected
+#if enable_gui:
+#    mygui_thread.start()
